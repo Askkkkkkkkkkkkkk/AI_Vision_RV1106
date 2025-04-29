3c65d49e18469f8964720a21681e5040b9b90b2f
luckfox-eng29 <eng29@luckfox.com>
2025-04-29 14:53:55 +0800

    build.sh : Add libc type selection
    Signed-off-by: luckfox-eng29 <eng29@luckfox.com>

--- build.sh
+++ build.sh
@@ -16,6 +16,24 @@ if [ "$1" = "clean" ]; then
 	exit
 fi
 
+libc_options=("uclibc"
+	"glibc")
+
+PS3="Enter your choice [1-${#libc_options[@]}]: "
+
+select opt in "${libc_options[@]}"; do
+	if [[ -n "$opt" ]]; then
+		echo "You selected: $opt"
+		echo "你选择了: $opt"
+
+		libc_type="$opt"
+		break
+	else
+		echo "Invalid selection, please try again."
+		echo "无效的选择，请重新选择。"
+	fi
+done
+
 options=("luckfox_pico_rtsp_opencv"
 	"luckfox_pico_rtsp_opencv_capture"
 	"luckfox_pico_rtsp_retinaface"
@@ -36,7 +54,7 @@ select opt in "${options[@]}"; do
 			fi
 			mkdir ${ROOT_PWD}/build
 			cd ${ROOT_PWD}/build
-			cmake .. -DEXAMPLE_DIR="$src_dir" -DEXAMPLE_NAME="$opt"
+			cmake .. -DEXAMPLE_DIR="$src_dir" -DEXAMPLE_NAME="$opt" -DLIBC_TYPE="$libc_type"
 			make install
 		else
 			echo "错误：目录 $src_dir 不存在！"
